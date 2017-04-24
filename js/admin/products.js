4f725cc268aa741fdb4d9a00224e2bb0fb4767ef
selik44 <dudselik44@gmail.com>
2017-04-24 17:02:02 +0300

    'y'

--- js/admin/products.js
+++ js/admin/products.js
@@ -791,12 +791,8 @@ product_tabs['Prices'] = new function(){
 	};
 }
 
-product_tabs['Associations'] = new function(){https://www.youtube.com/watch?v=nDVbQjBeULQ
+product_tabs['Associations'] = new function(){
 
-
-
-
-	
 	var self = this;
 	this.initAccessoriesAutocomplete = function (){
 		$('#product_autocomplete_input')
